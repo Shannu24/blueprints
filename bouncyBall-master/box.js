4358ce1c6b51c7b95908da658714a37b71c0c9dd
Shannu24 <74544880+Shannu24@users.noreply.github.com>
2020-12-20 14:57:25 +0530

    Add files via upload

---
+++ bouncyBall-master/box.js
@@ -0,0 +1,26 @@
+class Box{
+
+    constructor(x,y,width,height){
+
+        var options = {
+            restitution:2
+        }
+this.box=Bodies.rectangle(x,y,width,height,options)
+this.width=width;
+this.height=height;
+World.add(world,this.box);
+    }
+
+    display(){
+        var pos=this.box.position
+            rectMode(CENTER);
+            rect(pos.x,pos.y,this.width,this.height);
+
+
+
+    }
+
+
+
+
+}
